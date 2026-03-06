6b1ac45a799d5a7a0c66c1686f790194ec61f7b5
VIVEK9902 <vivekkumar258728@gmail.com>
2026-03-06 20:49:50 +0530

    fix cors

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -13,10 +13,12 @@ public class WebConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
+
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173") // React dev server
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedOriginPatterns("*")   // CHANGE THIS
+                        .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
                         .allowCredentials(true);
+
             }
         };
     }
